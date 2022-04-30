634fb76c78feec39550a4d2faa3516e2dca4fc6e
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-05-01 00:52:27 +0300

    Lesson_9_Response_and_response_model

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Query, Body, Path
-from schemas import Book, Author
+from schemas import Book, BookOut
 from typing import List
 
 app = FastAPI()
 
+
+@app.post('/book', response_model=BookOut)
+def create_book(item: Book):
+	book = item.dict()
+	book['id'] = 3
+	return book
 
 # @app.get('/')
 # def home():
@@ -18,18 +24,18 @@ app = FastAPI()
 #     return {'user': pk, 'item': item}
 
 
-@app.post('/book')
-def create_book(item: Book, author: Author, quantity: int = Body(...)):
-	return {'item': item, 'author': author, 'quantity': quantity}
+# @app.post('/book')
+# def create_book(item: Book, author: Author, quantity: int = Body(...)):
+# 	return {'item': item, 'author': author, 'quantity': quantity}
 
-@app.post('/author')
-def create_author(author: Author = Body(..., embed=True)):
-	return {'author': author}
+# @app.post('/author')
+# def create_author(author: Author = Body(..., embed=True)):
+# 	return {'author': author}
 
 
-@app.get('/book')
-def get_book(q: List[str] = Query(["test", "test2"], description="Search book", deprecated=True)):
-	return q
+# @app.get('/book')
+# def get_book(q: List[str] = Query(["test", "test2"], description="Search book", deprecated=True)):
+# 	return q
 
 # @app.get('/book/{pk}')
 # def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
--- schemas.py
+++ schemas.py
@@ -6,10 +6,10 @@ class Genre(BaseModel):
 	name: str
 
 
-class Author(BaseModel):
-	first_name: str = Field(..., max_length=25)
-	last_name: str
-	age: int = Field(..., gt=15, lt=90, description='Author age must be more than 15 and less than 90')
+# class Author(BaseModel):
+# 	first_name: str = Field(..., max_length=25)
+# 	last_name: str
+# 	age: int = Field(..., gt=15, lt=90, description='Author age must be more than 15 and less than 90')
 
 	# @validator('age')
 	# def check_age(cls, v):
@@ -24,5 +24,8 @@ class Book(BaseModel):
 	duration: str
 	data: date
 	summary: str
-	genres: List[Genre]
+	genres: List[Genre] = []
 	pages: int
+
+class BookOut(Book):
+	id: int
