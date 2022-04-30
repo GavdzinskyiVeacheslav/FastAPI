120e382f4e6b38d64e3c82cb2c444d1c63f251cb
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-30 22:32:37 +0300

    'Lesson_7_Field_validator_in_pydantic'

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import date
 
 class Genre(BaseModel):
@@ -10,6 +10,12 @@ class Author(BaseModel):
 	first_name: str
 	last_name: str
 	age: int
+
+	@validator('age')
+	def check_age(cls, v):
+		if v < 15:
+			raise ValueError('Author age must be more than 15')
+		return v
 
 
 class Book(BaseModel):
