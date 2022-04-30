692de94507fa5bbb55b71a82ea25119c1bf8bb73
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-30 19:18:42 +0300

    'Lesson_4_Query_parameters_and_validation'

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from schemas import Book
+from typing import List
 
 app = FastAPI()
 
 
-@app.get('/')
-def home():
-    return {'key' : 'Hello'}
+# @app.get('/')
+# def home():
+#     return {'key' : 'Hello'}
 
-@app.get('/{pk}')
-def get_item(pk: int, q: int = None):
-    return {"key": pk, 'q': q}
+# @app.get('/{pk}')
+# def get_item(pk: int, q: int = None):
+#     return {"key": pk, 'q': q}
 
-@app.get('/user/{pk}/items/{item}/')
-def get_user_item(pk: int, item: str):
-    return {'user': pk, 'item': item}
+# @app.get('/user/{pk}/items/{item}/')
+# def get_user_item(pk: int, item: str):
+#     return {'user': pk, 'item': item}
 
 
 @app.post('/book')
 def create_book(item: Book):
-	pass
+	return item
 
+
+@app.get('/book')
+def get_book(q: List[str] = Query(["test", "test2"], description="Search book", deprecated=True)):
+	return q
